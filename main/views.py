e22ccc13bcdc75fe5fd545d9fa9f78974b9cba40
MarrCre <marian.cretescu@gmail.com>
2024-06-04 08:58:56 +0300

    setup urls and view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {'name':'Excursion'}
+    retrun render(request, 'excursion\main\templates\main\index.html', context)
